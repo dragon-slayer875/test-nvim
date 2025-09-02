7b56c832ee2ec0774c2269f4cb4b501a21298358
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-09-02 10:31:53 +0530

    replace telescope with mini pick
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -2,14 +2,13 @@ return {
   -- Main LSP Configuration
   'neovim/nvim-lspconfig',
   dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
     -- Mason must be loaded before its dependents so we need to set it up here.
     -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
     { 'mason-org/mason.nvim', opts = {} },
     'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     -- Useful status updates for LSP.
-    { 'j-hui/fidget.nvim', opts = {} },
+    { 'j-hui/fidget.nvim', opts = {}, event = { 'BufReadPost' } },
     -- Allows extra capabilities provided by blink.cmp
     'saghen/blink.cmp',
   },
@@ -17,70 +16,43 @@ return {
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
-        -- In this case, we create a function that lets us more easily define mappings specific
-        -- for LSP related items. It sets the mode, buffer and description for us each time.
         local map = function(keys, func, desc, mode)
           mode = mode or 'n'
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        -- Rename the variable under your cursor.
-        --  Most Language Servers support renaming across files, etc.
         map('<leader>lr', vim.lsp.buf.rename, 'Rename current symbol')
 
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
         map('<leader>la', vim.lsp.buf.code_action, 'LSP code action', { 'n', 'x' })
 
         -- Find references for the word under your cursor.
-        map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('<leader>fr', "<cmd>Pick lsp scope='references'<CR>", 'Find references')
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map('<leader>fi', "<cmd>Pick lsp scope='implementation'<CR>", '[G]oto [I]mplementation')
 
         -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        map('gd', vim.lsp.buf.definition, 'Goto Definition')
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
-        map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map('<leader>ls', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
+        map('<leader>ls', "<cmd>Pick lsp scope='document_symbol'<CR>", 'Document Symbols')
 
         -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        map('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
+        map('<leader>lw', "<cmd>Pick lsp scope='workspace_symbol'<CR>", 'Workspace Symbols')
 
         -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        map('gt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+        map('gt', '<cmd>Pick lsp scope=type_definition<CR>', '[G]oto [T]ype Definition')
 
-        -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-        ---@param client vim.lsp.Client
-        ---@param method vim.lsp.protocol.Method
-        ---@param bufnr? integer some lsp support methods only in specific files
-        ---@return boolean
-        local function client_supports_method(client, method, bufnr)
-          if vim.fn.has 'nvim-0.11' == 1 then
-            return client:supports_method(method, bufnr)
-          else
-            return client.supports_method(method, { bufnr = bufnr })
-          end
-        end
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
@@ -106,8 +78,7 @@ return {
         -- The following code creates a keymap to toggle inlay hints in your
         -- code, if the language server you are using supports them
         --
-        -- This may be unwanted, since they displace some of your code
-        -- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+        -- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
         --   map('<leader>th', function()
         --     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
         --   end, '[T]oggle Inlay [H]ints')
@@ -116,9 +87,10 @@ return {
     })
 
     vim.diagnostic.config {
+      virtual_lines = { current_line = true },
       severity_sort = true,
       float = { border = 'rounded', source = 'if_many' },
-      underline = { severity = vim.diagnostic.severity.ERROR },
+      underline = false,
       signs = vim.g.have_nerd_font and {
         text = {
           [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -127,19 +99,6 @@ return {
           [vim.diagnostic.severity.HINT] = '󰌶 ',
         },
       } or {},
-      virtual_text = {
-        source = 'if_many',
-        spacing = 2,
-        format = function(diagnostic)
-          local diagnostic_message = {
-            [vim.diagnostic.severity.ERROR] = diagnostic.message,
-            [vim.diagnostic.severity.WARN] = diagnostic.message,
-            [vim.diagnostic.severity.INFO] = diagnostic.message,
-            [vim.diagnostic.severity.HINT] = diagnostic.message,
-          }
-          return diagnostic_message[diagnostic.severity]
-        end,
-      },
     }
 
     -- LSP servers and clients are able to communicate to each other what features they support.
@@ -160,10 +119,7 @@ return {
     local servers = {
       gopls = {},
       rust_analyzer = {},
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
       ts_ls = {},
-      --
-
       stylua = {},
       lua_ls = {
         -- cmd = { ... },
--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -5,14 +5,15 @@ return {
       'echasnovski/mini.extra',
       opts = {},
     },
-    keymap = {
-      set = function()
-        vim.keymap.set({ 'v', 'n' }, '<leader>fh', MiniPick.builtin.help, { desc = 'Find Help' })
-        vim.keymap.set({ 'v', 'n' }, '<leader>fb', MiniPick.builtin.buffers, { desc = 'Find Buffers' })
-        vim.keymap.set({ 'v', 'n' }, '<leader>ff', MiniPick.builtin.files, { desc = 'Find Files' })
-        vim.keymap.set({ 'v', 'n' }, '<leader>fw', MiniPick.builtin.grep, { desc = 'Find Words' })
-        vim.keymap.set({ 'v', 'n' }, '<leader>fr', MiniPick.builtin.resume, { desc = 'Find Resume' })
-      end,
+    keys = {
+      { '<leader>fh', ':Pick help<CR>', desc = 'Find Help', mode = { 'n' } },
+      { '<leader>fb', ':Pick buffers<CR>', desc = 'Find Buffers', mode = { 'n' } },
+      { '<leader>ff', ':Pick files<CR>', desc = 'Find Files', mode = { 'n' } },
+      { '<leader>fw', ':Pick grep_live<CR>', desc = 'Find Words', mode = { 'n' } },
+      { '<leader>fR', ':Pick resume<CR>', desc = 'Find Resume', mode = { 'n' } },
+      { '<leader>fc', ':Pick colorschemes<CR>', desc = 'Find Colorscheme', mode = { 'n' } },
+      { '<leader>fk', ':Pick keymaps<CR>', desc = 'Find Keymaps', mode = { 'n' } },
+      { '<leader>fd', ':Pick diagnostic scope=current<CR>', desc = 'Find Diagnostics', mode = { 'n' } },
     },
     opts = {},
   },
@@ -81,37 +82,3 @@ return {
     },
   },
 }
-
---     --   -- vim.keymap.set('n', '<leader>sh', MiniPick.builtin.help_tags, { desc = '[S]earch [H]elp' })
---     --   -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
---     --   -- vim.keymap.set('n', '<leader>sf', MiniPick.builtin.find_files, { desc = '[S]earch [F]iles' })
---     --   -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
---     --   -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
---     --   -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
---     --   -- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
---     --   -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
---     --   -- vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
---     --   vim.keymap.set('n', '<leader><leader>', MiniPick.builtin.buffers(), { desc = '[ ] Find existing buffers' }),
---     --   --
---     --   -- -- Slightly advanced example of overriding default behavior and theme
---     --   -- vim.keymap.set('n', '<leadew>/', function()
---     --   --   -- You can pass additional configuration to Telescope to change the theme, layout, etc.
---     --   --   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
---     --   --     winblend = 10,
---     --   --     previewer = false,
---     --   --   })
---     --   -- end, { desc = '[/] Fuzzily search in current buffer' })
---     --   --
---     --   -- -- It's also possible to pass additional configuration options.
---     --   -- --  See `:help telescope.builtin.live_grep()` for information about particular keys
---     --   -- vim.keymap.set('n', '<leader>s/', function()
---     --   --   builtin.live_grep {
---     --   --     grep_open_files = true,
---     --   --     prompt_title = 'Live Grep in Open Files',
---     --   --   }
---     --   -- end, { desc = '[S]earch [/] in Open Files' })
---     --   --
---     --   -- -- Shortcut for searching your Neovim configuration files
---     --   -- vim.keymap.set('n', '<leader>sn', function()
---     --   --   builtin.find_files { cwd = vim.fn.stdpath 'config' }
---     --   -- end, { desc = '[S]earch [N]eovim files' })
